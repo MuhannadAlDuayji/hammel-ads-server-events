cf89f5292510b9d46c9b20dabcf4837f25b1f3be
Firas Melliti <mellitifiras@protonmail.com>
2023-08-25 22:25:14 +0100

    adding all of the datasets logic to the event queue

--- src/utils/EventQueue.ts
+++ src/utils/EventQueue.ts
@@ -4,8 +4,10 @@ import {
     CampaignStatusId,
     CampaignStatusName,
 } from "../types/campaign/CampaignStatus";
-import { EventTypeId } from "../types/event/EventType";
+import { EventTypeId, EventTypeName } from "../types/event/EventType";
 import User from "../models/UserSchema";
+import { MongoClient } from "mongodb";
+import Dataset from "../types/dataset";
 
 class EventQueue {
     private events: Event[];
@@ -27,14 +29,113 @@ class EventQueue {
     }
 
     private async saveEvents() {
-        if (this.events.length === 0) {
-            return;
-        }
+        // if (this.events.length === 0) {
+        //     return;
+        // }
 
         const eventsToSave = this.events.slice();
         this.events = [];
 
         try {
+            // -------------- save datasets --------------
+            const client = new MongoClient(process.env.URI_STRING || "");
+            const db = client.db();
+            const timeSeriesCollection = db.collection("eventsTimeSeries");
+            const groupedEvents: Event[][] = this.groupEvents(eventsToSave);
+            const now = new Date();
+            const { startOfDay, endOfDay } = this.getDay(now);
+
+            const newDatasetsToInsert: Dataset[] = [];
+            for (let i = 0; i < groupedEvents.length; i++) {
+                const group: Event[] = groupedEvents[i];
+                const views: Event[] = group.filter(
+                    (event: Event) => event.eventTypeId === EventTypeId.VIEW
+                );
+                const clicks: Event[] = group.filter(
+                    (event: Event) => event.eventTypeId === EventTypeId.CLICK
+                );
+
+                const closes: Event[] = group.filter(
+                    (event: Event) => event.eventTypeId === EventTypeId.CLOSE
+                );
+                const groupProprities = {
+                    campaignId: group[0].campaignId,
+                    country: group[0].country,
+                    city: group[0].city,
+                };
+                const existingDataset = await timeSeriesCollection.findOne({
+                    createdAt: { $gte: startOfDay, $lte: endOfDay },
+                    campaignId: groupProprities.campaignId,
+                    country: groupProprities.country,
+                    city: groupProprities.city,
+                });
+
+                if (!existingDataset) {
+                    const newDataset: Dataset = {
+                        createdAt: new Date(),
+                        campaignId: groupProprities.campaignId,
+                        country: groupProprities.country,
+                        city: groupProprities.city,
+                        views: 0,
+                        clicks: 0,
+                        closes: 0,
+                        averageClickWatchTime: null,
+                        averageCloseWatchTime: null,
+                    };
+
+                    newDataset.views += views.length;
+                    newDataset.clicks += clicks.length;
+                    newDataset.closes += closes.length;
+
+                    newDataset.averageClickWatchTime =
+                        this.getAverageWatchTime(clicks);
+                    newDataset.averageCloseWatchTime =
+                        this.getAverageWatchTime(closes);
+
+                    newDatasetsToInsert.push(newDataset);
+                } else {
+                    existingDataset.views += views.length;
+
+                    if (existingDataset.averageClickWatchTime === null) {
+                        existingDataset.averageClickWatchTime =
+                            this.getAverageWatchTime(clicks);
+                    } else {
+                        const newAverageWatchTime =
+                            (existingDataset.averageClickWatchTime *
+                                existingDataset.clicks +
+                                (this.getAverageWatchTime(clicks) || 0)) /
+                            (existingDataset.clicks + clicks.length);
+
+                        existingDataset.averageClickWatchTime =
+                            newAverageWatchTime;
+                    }
+                    existingDataset.clicks += clicks.length;
+                    if (existingDataset.averageCloseWatchTime === null) {
+                        existingDataset.averageCloseWatchTime =
+                            this.getAverageWatchTime(closes);
+                    } else {
+                        const newAverageWatchTime =
+                            (existingDataset.averageCloseWatchTime *
+                                existingDataset.closes +
+                                (this.getAverageWatchTime(closes) || 0)) /
+                            (existingDataset.closes + closes.length);
+
+                        existingDataset.averageCloseWatchTime =
+                            newAverageWatchTime;
+                    }
+                    existingDataset.closes += closes.length;
+
+                    await timeSeriesCollection.updateOne(
+                        { _id: existingDataset._id },
+                        { $set: existingDataset }
+                    );
+                }
+            }
+
+            if (newDatasetsToInsert.length > 0) {
+                await timeSeriesCollection.insertMany(newDatasetsToInsert);
+            }
+            // -------------- update campaign information ------------
             const campaigns = [
                 ...new Set(eventsToSave.map((event) => event.campaignId)),
             ];
@@ -79,7 +180,6 @@ class EventQueue {
                     cost += oneViewCost - oneViewCost * view.discount;
                 }
             }
-
             const campaign = await CampaignSchema.findByIdAndUpdate(
                 campaignId,
                 {
@@ -91,11 +191,9 @@ class EventQueue {
                 },
                 { new: true }
             );
-
             if (!campaign) {
                 return false;
             }
-
             const currentBalance: number | null = await this.chargeUser(
                 campaign.userId,
                 cost
@@ -124,12 +222,6 @@ class EventQueue {
                 });
             }
 
-            // // make the load served
-            // await load.updateOne({
-            //     loadStatusId: LoadStatusId.SERVED,
-            //     loadStatusName: LoadStatusName.SERVED,
-            // });
-
             await campaign.updateOne({
                 $inc: {
                     pendingCount: -views.length,
@@ -137,16 +229,20 @@ class EventQueue {
                 },
             });
             await campaign.save();
+            return true;
         } catch (err) {
             console.log("errorr ", err);
-            return false;
+            return null;
         }
     }
 
     private async chargeUser(userId: string, cost: number) {
         try {
             const user = await User.findById(userId);
-            if (!user) return null;
+            if (!user) {
+                console.log("user not found");
+                return null;
+            }
 
             if (user.balance - cost < 0) {
                 user.balance = 0;
@@ -157,10 +253,49 @@ class EventQueue {
             await user.save();
             return user.balance;
         } catch (err) {
-            console.log("error ", err);
+            console.log(err);
             return null;
         }
     }
+
+    private groupEvents(events: Event[]) {
+        const groupedEvents: any = {};
+        for (const event of events) {
+            const { campaignId, country, city } = event;
+            const key = `${campaignId}-${country}-${city}`;
+
+            if (!groupedEvents[key]) {
+                groupedEvents[key] = [];
+            }
+
+            groupedEvents[key].push(event);
+        }
+        const result: Event[][] = Object.values(groupedEvents);
+
+        return result;
+    }
+    private getDay = (date: Date) => {
+        const startOfDay = new Date(date);
+        startOfDay.setHours(0, 0, 0, 0);
+
+        const endOfDay = new Date(date);
+        endOfDay.setHours(23, 59, 59, 999);
+
+        return { startOfDay, endOfDay };
+    };
+
+    private getAverageWatchTime = (events: Event[]) => {
+        if (events.length === 0) return null;
+        let totalWatchTime = 0;
+        for (const event of events) {
+            totalWatchTime += event.watchTime || 0; // Ensure you handle null values
+        }
+
+        const numberOfEvents = events.length;
+        const averageWatchTime = totalWatchTime / numberOfEvents;
+
+        return averageWatchTime;
+    };
 }
 
 export default new EventQueue();
